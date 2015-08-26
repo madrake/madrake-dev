e3f9d8a3890df222eb14e37fe822a92010558381
madrake <madrake@gmail.com>
2015-08-25 17:41:35 -0700

    Test update from GitHub Desktop
    Trivial TODO change

--- src/madrake/BigMoneys.java
+++ src/madrake/BigMoneys.java
@@ -6,7 +6,7 @@ public final class BigMoneys {
 
   private BigMoneys() { /* do not instantiate */ }
   
-  // TODO(madrake): there must be a better way to do this
+  // TODO(madrake): Find a better way to do this
   public static final boolean equals(BigMoney first, BigMoney second) {
     if (first == null && second == null) {
       return true;
